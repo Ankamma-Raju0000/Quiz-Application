68ef2136a9136d72a7206f4c5418e29aef833f2c
Ankamma-Raju0000 <ankammaraju0000@gmail.com>
2025-11-06 18:44:11 +0000

    Create Response.java

---
+++ Response.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response 
+{
+	private int idl;
+	private String response;
+	
+	
+
+}
